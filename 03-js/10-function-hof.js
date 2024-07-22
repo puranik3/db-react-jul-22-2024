b02fdcc7045030d4b5f785db7f6a61a805f44ffc
Prashanth Puranik <puranik@digdeeper.in>
2024-07-22 16:58:02 +0530

    correction in exercise function name

--- 03-js/10-function-hof.js
+++ 03-js/10-function-hof.js
@@ -3,6 +3,9 @@ function sumArray(arr) {
 
 }
 
-console.log(sumArr([1, 2, 3, 4])); // 10
+// prints the sum - 10
+console.log(
+    sumArray([1, 2, 3, 4])
+);
 
 // You can return a function from a function
