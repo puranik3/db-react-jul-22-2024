ed5a9e320f33a1b1b8351e41dc71adbe29f02764
Prashanth Puranik <puranik@digdeeper.in>
2024-07-24 12:21:42 +0530

    Extracted user input

--- 04-react/workshops-app/src/components/AddSession.tsx
+++ 04-react/workshops-app/src/components/AddSession.tsx
@@ -1,41 +1,97 @@
+import { FormEvent, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 interface IProps {
     workshopId: number;
 }
 
-const AddSession = (props: IProps) => {
+// Pattern 2: Controlled components
+// destructuring the props object and creating a variable called workshopId
+const AddSession = ({ workshopId }: IProps) => {
+    // you create a state for every input
+    const [sequenceId, setSequenceId] = useState("10");
+    const [name, setName] = useState("");
+    const [speaker, setSpeaker] = useState("");
+    const [duration, setDuration] = useState("");
+    const [level, setLevel] = useState("");
+    const [abstract, setAbstract] = useState("");
+
+    function addSession(event: FormEvent) {
+        event.preventDefault();
+
+        const newWorkshop = {
+            // workshopId: workshopId
+            // sequenceId: sequenceId
+            workshopId,
+            sequenceId: +sequenceId,
+            name,
+            speaker,
+            duration: +duration,
+            level,
+            abstract,
+            upvoteCount: 0,
+        };
+
+        console.log(newWorkshop);
+
+        // ready to send this to the backend
+    }
+
     return (
         <div>
             <h2>Add a new session</h2>
 
             <hr />
 
-            <Form>
+            <Form onSubmit={addSession}>
                 <Form.Group className="mb-3" controlId="sequenceId">
                     <Form.Label>Sequence ID</Form.Label>{" "}
                     {/* for of label is htmlFor in JSX */}
-                    <Form.Control type="text" placeholder="Sequence ID" />
+                    <Form.Control
+                        type="text"
+                        placeholder="Sequence ID"
+                        value={sequenceId}
+                        onChange={(event) => setSequenceId(event.target.value)}
+                    />
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="password">
+                <Form.Group className="mb-3" controlId="name">
                     <Form.Label>Name</Form.Label>
-                    <Form.Control type="text" placeholder="New session name" />
+                    <Form.Control
+                        type="text"
+                        placeholder="New session name"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="speaker">
                     <Form.Label>Speaker</Form.Label>
-                    <Form.Control type="text" placeholder="John Doe" />
+                    <Form.Control
+                        type="text"
+                        placeholder="John Doe"
+                        value={speaker}
+                        onChange={(event) => setSpeaker(event.target.value)}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="duration">
                     <Form.Label>Duration</Form.Label>
-                    <Form.Control type="text" placeholder="1.5" />
+                    <Form.Control
+                        type="text"
+                        placeholder="1.5"
+                        value={duration}
+                        onChange={(event) => setDuration(event.target.value)}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="level">
                     <Form.Label>Level</Form.Label>
-                    <Form.Select aria-label="Level of the session">
+                    <Form.Select
+                        aria-label="Level of the session"
+                        value={sequenceId}
+                        onChange={(event) => setLevel(event.target.value)}
+                    >
                         <option>Select a level</option>
                         <option>Basic</option>
                         <option>Intermediate</option>
@@ -45,7 +101,11 @@ const AddSession = (props: IProps) => {
 
                 <Form.Group className="mb-3" controlId="abstract">
                     <Form.Label>Abstract</Form.Label>
-                    <Form.Control as="textarea" />
+                    <Form.Control
+                        as="textarea"
+                        value={abstract}
+                        onChange={(event) => setAbstract(event.target.value)}
+                    />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
